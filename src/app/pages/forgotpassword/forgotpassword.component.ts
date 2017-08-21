a979a3a59341a0d085d9e6d95ad4cb81a23f6506
Phat Hong Phung <phat.hong@newoceaninfosys.com>
2017-08-21 15:59:24 +0700

    Fix password compare

--- src/app/pages/forgotpassword/forgotpassword.component.ts
+++ src/app/pages/forgotpassword/forgotpassword.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { FormGroup, AbstractControl, FormBuilder, Validators } from '@angular/forms';
 
 import { AppConstant } from "../../app.constant";
+import { AuthService } from "app/shared/services";
+import { ToastrService } from "ngx-toastr";
+import { Route, Router } from "@angular/router";
 
 @Component({
   selector: 'forgotpassword',
@@ -10,11 +13,14 @@ import { AppConstant } from "../../app.constant";
 })
 export class ForgotPassword {
 
-  public frm:FormGroup;
-  public email:AbstractControl;
-  public submitted:boolean = false;
+  public frm: FormGroup;
+  public email: AbstractControl;
+  public submitted: boolean = false;
 
-  constructor(fb:FormBuilder) {
+  constructor(fb: FormBuilder,
+              private _auth: AuthService,
+              private _router: Router,
+              private _toast: ToastrService) {
     this.frm = fb.group({
       'email': ['', Validators.compose([Validators.required, Validators.pattern(AppConstant.pattern.email)])],
     });
@@ -22,9 +28,20 @@ export class ForgotPassword {
     this.email = this.frm.controls['email'];
   }
 
-  public onSubmit(values:Object):void {
+  public onSubmit(formValue: any): void {
     this.submitted = true;
     if (this.frm.valid) {
+      this._auth.forgotPassword({
+        email: formValue.email
+      }).subscribe((resp) => {
+
+        this._router.navigate(['page', 'login']);
+        this._toast.success('An email has been sent to the address on file covering how to reset your password');
+        // this._toast.success("Your password has successful reset", "Success");
+        // this._router.navigate(['page', 'login']);
+      }, (err: any) => {
+        this.submitted = false;
+      });
       // your code goes here
       // console.log(values);
     }
--- src/app/theme/validators/equalPasswords.validator.ts
+++ src/app/theme/validators/equalPasswords.validator.ts
@@ -3,11 +3,10 @@ import { FormGroup } from '@angular/forms';
 export class EqualPasswordsValidator {
 
   public static validate(firstField, secondField) {
-
     return (c: FormGroup) => {
-      return c.controls[secondField].setErrors({
-        passwordsEqual: (c.controls && c.controls[firstField].value == c.controls[secondField].value)
-      });
+      return (c.controls && c.controls[firstField].value == c.controls[secondField].value) ? c.controls[secondField].setErrors(null) : c.controls[secondField].setErrors({
+        passwordsEqual: true
+      })
     }
   }
 }

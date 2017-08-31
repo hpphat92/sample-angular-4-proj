ca762eb0c609aa22384d3c45d2f5c392ead83abe
Phat Hong Phung <phat.hong@newoceaninfosys.com>
2017-08-31 14:12:55 +0700

    Fix #56

--- src/app/app.constant.ts
+++ src/app/app.constant.ts
@@ -1,6 +1,6 @@
 export const AppConstant = {
-  // domain: 'http://115.75.6.162:44459',
-  domain: 'https://alythexapi.azurewebsites.net',
+  domain: 'http://115.75.6.162:44459',
+  // domain: 'https://alythexapi.azurewebsites.net',
   format: {
     moment: {
       shortDate: 'YYYY-MM-DD',

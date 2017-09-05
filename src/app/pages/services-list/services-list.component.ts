b1e1ba2c44fcf513e8dc56f9973bea0f94c946ff
QuanVu <quan.vu@newoceaninfosys.com>
2017-09-05 16:43:36 +0700

    fix: 65

--- src/app/pages/services-list/services-list.component.ts
+++ src/app/pages/services-list/services-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChildren, QueryList, ElementRef } from '@angular/core';
 import { AppConstant } from "../../app.constant";
 import { ExtendedHttpService } from "../../shared/services/http/http.service";
 import { GlobalState } from "../../global.state";
@@ -8,13 +8,17 @@ import { GlobalState } from "../../global.state";
   styleUrls: ['./services-list.scss'],
   templateUrl: './services-list.html'
 })
-export class ServicesList {
+export class ServicesList implements AfterViewInit {
   public items = [];
   public position = {
     position: 'top cursor'
   };
 
-  constructor(private _http: ExtendedHttpService, private _state: GlobalState) {
+  @ViewChildren('allTheseThings') things: QueryList<any>;
+
+  constructor(private _http: ExtendedHttpService, 
+              private _state: GlobalState,
+              private _elementRef: ElementRef) {
     this._http.get(`${AppConstant.domain}/w-api/services/all`).map((json) => json.json()).subscribe((resp) => {
       this.items = resp.data;
       // this._state.notifyDataChanged('menu.activeLink', {
@@ -23,4 +27,22 @@ export class ServicesList {
     });
   }
 
+  public ngAfterViewInit(): void {
+    this.things.changes.subscribe(t => {
+      let iframes = Array.from(this._elementRef.nativeElement.querySelectorAll('iframe'));
+      if (!iframes.length) {
+        return;
+      }
+      this.items.forEach((service) => {
+        let iframe = iframes.find((i) => service.id == (i as any).getAttribute('id'));
+        if (iframe) {
+          (iframe as any).contentDocument.open();
+          (iframe as any).contentDocument.write(service.description);
+          (iframe as any).contentDocument.close();
+          (iframe as any).width = (iframe as any).contentWindow.document.body.scrollWidth;
+          (iframe as any).height = (iframe as any).contentWindow.document.body.scrollHeight;
+        }
+      })
+    })
+  }
 }

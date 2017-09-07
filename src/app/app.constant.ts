213c95fd0181255a2878dcb6a4243b3bb273ea53
Phat Hong Phung <phat.hong@newoceaninfosys.com>
2017-09-07 10:22:37 +0700

    fix css

--- src/app/app.constant.ts
+++ src/app/app.constant.ts
@@ -1,7 +1,7 @@
 export const AppConstant = {
-  // domain: 'http://115.75.6.162:44459',
+  domain: 'http://115.75.6.162:44459',
   // domain: 'http://localhost:55948',
-  domain: 'https://alythexapi.azurewebsites.net',
+  // domain: 'https://alythexapi.azurewebsites.net',
   format: {
     moment: {
       shortDate: 'YYYY-MM-DD',
@@ -10,7 +10,7 @@ export const AppConstant = {
   },
   pattern: {
     email: '[A-Za-z0-9]+([\\._-][A-Za-z0-9]+)*@[A-Za-z0-9]+([\\.-][A-Za-z0-9]+)*(\\.[A-Za-z]{2,4})',
-    phone: '(\\+\\d{1,2})?[\\(\\s.-]?\\d{1,4}[\\)\\s.-]?[\\s.-]?\\d{1,4}[\\s.-]?\\d{1,4}'
+    phone: '[\\d\\s]+'
   },
   imageUrl: ''
 }

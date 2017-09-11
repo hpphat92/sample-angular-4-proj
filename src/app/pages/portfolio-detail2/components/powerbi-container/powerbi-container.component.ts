8c4f3336c1ad5a82127b8eefe96b079c15c9a084
Phat Hong Phung <phat.hong@newoceaninfosys.com>
2017-09-11 15:37:55 +0700

    fix

--- src/app/pages/portfolio-detail2/components/powerbi-container/powerbi-container.component.ts
+++ src/app/pages/portfolio-detail2/components/powerbi-container/powerbi-container.component.ts
@@ -73,7 +73,6 @@ export class PowerbiContainerComponent implements AfterViewInit {
   }
 
   goFullScreen() {
-    this.iOS = true;
     this.isFullScreen = true;
     if(!this.iOS) {
       this.powerBiRecord.fullscreen();
